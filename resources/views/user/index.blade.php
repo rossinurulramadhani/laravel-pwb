b1ffb33eeaeee6beb1cba61becc25f8723c3c340
Rossi nurul ramadhani ochi <ochirossi062@gmail.com>
2023-08-25 13:59:49 +0700

    update

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,21 @@
+<html>
+  <head> </head>
+  <body>
+    <h1>Media Online</h1>
+    <h3>Sosial Media Developer</h3>
+    <p>Belajar dan berbagi agar hidup menjadi lebih baik</p>
+    <h4>Benefit Join di Media Online</h4>
+    <ul>
+      <li>mendapatkan motivasi dari sesama para Developer</li>
+      <li>Sharing knowlenge</li>
+      <li>Dibuat oleh calon web developer terbaik</li>
+    </ul>
+    <h4>Cara bergabung ke Media Online</h4>
+    <ol>
+      <li>Mengunjungi Website ini</li>
+      <li>Medaftarkan di <a href="/form">form Sign Up</a></li>
+
+      <li>Selesai</li>
+    </ol>
+  </body>
+</html>
---
+++ resources/views/user/welcome.blade.php
@@ -0,0 +1,6 @@
+<html>
+  <h1>SELAMAT DATANG!</h1>
+  <h5>
+    Terima kasih telah bergabung di Website Kami. Media Belajar kita bersama
+  </h5>
+</html>
